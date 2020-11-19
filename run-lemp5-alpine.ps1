630d95afcc2874166e0db5cacf54319630566e83
Miloud Maamar <development@maamar.fr>
2020-11-19 09:36:28 +0100

    fix build path

--- run-lemp5-alpine.ps1
+++ run-lemp5-alpine.ps1
@@ -1,4 +1,4 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker build $(Get-Location)/. -t crazyyoshi/lemp5-alpine:latest
+docker build $PSScriptRoot  -t crazyyoshi/lemp5-alpine:latest
 docker run -d -p 80:80 crazyyoshi/lemp5-alpine:latest
